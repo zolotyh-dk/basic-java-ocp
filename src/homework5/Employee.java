d5d6c1bb4e02cf3772f05ada33b87cd640482db2
Dmitry Zolotyh <zolotyh.dk@gmail.com>
2022-05-12 16:12:21 +0300

    Chandge variables and constuctors access modifiers in homework5.Employee. Add 3 methods into Employee. Create class AccessModifierTest.

--- src/homework5/Employee.java
+++ src/homework5/Employee.java
@@ -2,9 +2,9 @@ package homework5;
 
 public class Employee {
     int id;
-    String surname;
+    public String surname;
     int age;
-    double salary;
+    private double salary;
     String department;
 
     public Employee(int id, String surname, int age, double salary, String department) {
@@ -15,8 +15,28 @@ public class Employee {
         this.department = department;
     }
 
+    Employee(int id, String surname, int age) {
+        this(id, surname, age, 0.0, null);
+    }
+
+    private Employee(int id) {
+        this.id = id;
+    }
+
     void doubleSalary() {
         salary *= 2;
+    }
+
+    public void showID() {
+        System.out.println(id);
+    }
+
+    public void showSurname() {
+        System.out.println(surname);
+    }
+
+    public void showSalary() {
+        System.out.println(salary);
     }
 }
 
@@ -24,10 +44,12 @@ class EmployeeTest {
     public static void main(String[] args) {
         Employee taras = new Employee(1, "Тарас", 35, 100, "HR");
         taras.doubleSalary();
-        System.out.println("Новая зарплата работника " + taras.surname + " :" + taras.salary);
+        System.out.print("Новая зарплата работника " + taras.surname + " :");
+        taras.showSalary();
 
         Employee elena = new Employee(2, "Елена", 33, 120, "IT");
         elena.doubleSalary();
-        System.out.println("Новая зарплата работника " + elena.surname + " :" + elena.salary);
+        System.out.println("Новая зарплата работника " + elena.surname + " :");
+        elena.showSalary();
     }
 }
